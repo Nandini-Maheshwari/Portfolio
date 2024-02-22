ec3f22a058c3b86dcefee18934ae72385a03cbb7
Nandini-Maheshwar <nandinim1204@gmail.com>
2024-02-22 19:35:27 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Header from './Components/Header.jsx'
+import Footer from './Components/Footer.jsx'
+import Layout from './Components/Layout.jsx'
+import Home from './Components/Home.jsx'
+import About from './Components/About.jsx'
+import Projects from './Components/Projects.jsx'
+//import Contact from './Components/Contact.jsx'
 
-function App() {
-
-  return (
-    <>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path='About' element={<About />} />
+      <Route path='Projects' element={<Projects />} />
+      {/* <Route path='Contact' element={<Contact />} /> */}
+    </Route>
   )
-}
+)
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>  
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
