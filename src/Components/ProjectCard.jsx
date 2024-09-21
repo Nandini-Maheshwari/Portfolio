cd7a013ccd4109881229dd891d560746c6ef28f5
Nandini-Maheshwari <nandinim1204@gmail.com>
2024-09-21 17:55:30 +0530

    Added projects

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -3,9 +3,9 @@ import { GoArrowRight } from "react-icons/go";
 
 function ProjectCard({ image, altText, description, link }) {
   return (
-    <div className='flex flex-col text-white items-center justify-around'>
-      <img src={image} alt={altText} className='w-3/4 rounded-2xl'/>
-      <p className='pt-4 pb-2 text-lg'>
+    <div className='flex flex-col text-white items-center justify-around mb-20'>
+      <img src={image} alt={altText} className='w-3/5 max-h-90 rounded-2xl'/>
+      <p className='pt-4 pb-2 text-lg w-3/4'>
         <code className='text-yellow-300'>{description}</code>
       </p>
       <button className='rounded-3xl bg-neutral-700 hover:bg-neutral-500 py-2 px-4'>
--- src/Screens/Projects.jsx
+++ src/Screens/Projects.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import demo from '../assets/useHub.png';
+import music from '../assets/musicNextJS.png';
+import starSky from '../assets/XRStarSky.jpg';
+import devTube from '../assets/devTube.jpg';
 import ProjectCard from '../Components/ProjectCard.jsx';
 
 function Projects() {
   const projectDetails = [
     {
+      image: devTube,
+      altText: 'Project',
+      description: 'Designed a scalable backend for YouTube using Node.js, Express, and MongoDB.',
+      link: 'https://github.com/Nandini-Maheshwari/devTube'
+    },
+    {
+      image: music,
+      altText: 'Project',
+      description: 'An experimental Next.js website showcasing the integration of Aceternity UI and Shadcn for enhanced user experience and design.',
+      link: 'https://github.com/Nandini-Maheshwari/music-nextjs'
+    },
+    {
       image: demo,
-      altText: 'demo',
+      altText: 'Project',
       description: 'Website landing page demo, designed and developed using HTML and CSS.',
       link: 'https://github.com/Nandini-Maheshwari'
     },
-    // {
-    //   image: demo,
-    //   altText: 'project 2',
-    //   description: 'Another project description.',
-    //   link: 'https://github.com/your-repo2'
-    // }
+    {
+      image: starSky,
+      altText: 'Project',
+      description: 'A VR based informative game on sapce biology. Among NASA Space Apps 2022 Global Nominees.',
+      link: 'https://github.com/Nandini-Maheshwari/space-superhero'
+    },
   ];
 
   return (
-    <div className='py-24'>
+    <div className='pt-24'>
       {projectDetails.map((project, index) => (
         <ProjectCard 
           key={index}
